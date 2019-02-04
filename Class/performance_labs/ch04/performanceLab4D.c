d37edcf41447b7772c080d423296a6697d833a41
student <student@localhost.localdomain>
2019-02-04 14:17:26 -0600

    changes

--- Class/performance_labs/ch04/performanceLab4D.c
+++ Class/performance_labs/ch04/performanceLab4D.c
@@ -2,7 +2,16 @@
     Author: Jared Young
     Date: 04 FEB 2019
     Project: Performance Lab 4D
-    Description: 
+    Description: Part 1:
+                 Read a first, middle and last name as input into separate char arrays.
+                 Specify a field-width to protect against buffer overflow.
+                 Ensure the field-width leaves room for a nul-terminator.
+                 Specify a ("\t") as a delimitating character.
+                 Print the full name, separating each string with a tab and newline ("\t\n").
+                 Part 2:
+                 Read two integers from one line.
+                 Format the input so that the integers are separated by an asterisk (*) x*y (e.g., 2*3, 11*14).
+                 Reprint the two integers and result as if the answer were being read by a human.
 */
 #include <stdio.h>
 
