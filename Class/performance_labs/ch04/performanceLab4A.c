4e02c938e857dc91344030804544556db2aa0617
student <student@localhost.localdomain>
2019-02-04 13:10:06 -0600

    changes

--- Class/performance_labs/ch04/performanceLab4A.c
+++ Class/performance_labs/ch04/performanceLab4A.c
@@ -1,3 +1,10 @@
+/*
+    Author: Jared Young
+    Date: 04 FEB 2019
+    Project: Performance Lab 4A
+    Description: Input a single character and then print the next sequential character.
+*/
+
 #include <stdio.h>
 
 int main()
