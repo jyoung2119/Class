d6ca041c629985dfcc83d21c4b36e5cd75c57b21
student <student@localhost.localdomain>
2019-02-04 13:45:48 -0600

    changes

--- Class/performance_labs/ch04/performanceLab4C.c
+++ Class/performance_labs/ch04/performanceLab4C.c
@@ -1,3 +1,10 @@
+/*
+    Author: Jared Young
+    Date: 04 FEB 2019
+    Project: Performance Lab 4C
+    Description: Read a string from stdin into a char array of dimension 10.
+                 Use fgets() and fputs() to display the string.
+*/
 #include <stdio.h>
 
 int main()
