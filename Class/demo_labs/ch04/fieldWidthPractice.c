1c2c41c0ead5d58b81b9468fc24f67a22e8b3562
student <student@localhost.localdomain>
2019-02-07 13:31:48 -0600

    new

---
+++ Class/demo_labs/ch04/fieldWidthPractice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float someNum = 12.3456;
+
+    printf("%f\n", someNum);
+    printf("%0f\n", someNum);
+    printf("%2f\n", someNum);
+    printf("%4f\n", someNum);
+    printf("%8f\n", someNum);
+    printf("%16f\n", someNum);
+    printf("%25f\n", someNum);
+
+}
