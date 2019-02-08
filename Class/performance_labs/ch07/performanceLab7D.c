8e9ce0d51ee1789eac0c1e0d67cfdcf8e12acf33
student <student@localhost.localdomain>
2019-02-08 10:46:04 -0600

    new

---
+++ Class/performance_labs/ch07/performanceLab7D.c
@@ -0,0 +1,58 @@
+/*
+    Author: Jared Young
+    Date: 08 FEB 2019
+    Project: Performance Lab 7D
+    Description: Write a program that will evaluate a simple math formula.
+                 Input the formula with two integers separated by an arithmetic operator from one line.
+                    Example: 1 + 1, (-8) - 3, 2 * (-7), (-15) / (-4)
+                 Use a switch statement to print the correct value using the operators as cases.
+                 Set the default condition to an error.
+                 Ensure any division provides a double answer with a precision of two decimal places.
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    char mathOperator = 0;
+    int firstVariable = 0;
+    int secondVariable = 0;
+
+    printf("Input two numbers separated by an operator: ");
+    scanf("%d%c%d", &firstVariable, &mathOperator, &secondVariable);
+
+    switch(mathOperator)
+    {
+        //Addition
+        case '+':
+            printf("%d + %d = %d\n", firstVariable, secondVariable, firstVariable+secondVariable);
+            break;
+        //Subtraction
+        case '-':
+            printf("%d - %d = %d\n", firstVariable, secondVariable, firstVariable-secondVariable);
+            break;
+        //Multiplication
+        case '*':
+            printf("%d * %d = %d\n", firstVariable, secondVariable, firstVariable*secondVariable);
+            break;
+        //Division
+        case '/':
+            //Prevents dividing by zero
+            if(secondVariable != 0)
+            {
+                printf("%d / %d = %.2lf\n", firstVariable, secondVariable, (double)firstVariable/secondVariable);
+                break;
+            }
+            else
+            {
+                printf("ERROR: CAN'T DIVIDE BY 0 SCRUB\n");
+                break;
+            }
+        //Modulo 
+        case '%':
+            printf("%d %% %d = %d\n", firstVariable, secondVariable, firstVariable%secondVariable);
+            break;
+        //If person is illiterate
+        default:
+            printf("ERROR: YOU SUCK. TRY AGAIN.\n");
+    }   
+}
